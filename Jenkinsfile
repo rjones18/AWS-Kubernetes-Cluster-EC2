4733f9d2ef4e716846d22acb6df06341f0c36dd6
rjones18 <regjones16@outlook.com>
2023-10-31 19:43:36 -0500

    Add deployment code

---
+++ Jenkinsfile
@@ -0,0 +1,10 @@
+pipeline {
+    agents any
+    stages {
+        stage('Fetch code') {
+           steps {
+             git branch: 'main'
+           }
+        }
+    }
+}
--- application_code/app.py
+++ application_code/app.py
@@ -45,6 +45,6 @@ def speech():
     return Response(audio_stream, mimetype='audio/mpeg')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
 
 
